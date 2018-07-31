845f124fe1cae7f759f9b60363828e664988e890
Paul Holden <pholden@stria.com>
2018-07-31 16:57:52 -0700

    Update with info command

--- cmis/__init__.py
+++ cmis/__init__.py
@@ -1,13 +1,10 @@
-import sys
-import yaml
 from cmislib import CmisClient
 from cmislib.atompub.binding import AtomPubBinding
 
-cfg = yaml.load(file(sys.argv[1], 'r'))
+def info(cfg):
+    client = CmisClient(cfg['service_endpoint'], cfg['username'], cfg['password'], binding=AtomPubBinding())
+    repo = client.defaultRepository
+    info = repo.info
 
-client = CmisClient(cfg['service_endpoint'], cfg['username'], cfg['password'], binding=AtomPubBinding())
-repo = client.defaultRepository
-info = repo.info
-
-for k, v in info.items():
-    print '%s:%s' % (k, v)
+    for k, v in info.items():
+        print '%s:%s' % (k, v)
