cc9ca9826392965f0432409d92f376fd4fe1e8bc
Paul Holden <pholden@stria.com>
2018-07-31 15:11:52 -0700

    Renaming

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='caas-filenet',
+setup(name='caas-cmis',
       version='0.1',
-      description='CaaS FileNet services',
-      url='https://github.com/paulholden2/caas-filenet',
+      description='CaaS CMIS tools',
+      url='https://github.com/paulholden2/caas-cmis',
       author='Paul Holden',
       author_email='pholden@stria.com',
       license='MIT',
       install_requires=['PyYAML','watchtower','cmislib'],
-      packages=['filenet'],
-      scripts=['bin/filenet'],
+      packages=['cmis'],
+      scripts=['bin/cmis'],
       zip_safe=False)
