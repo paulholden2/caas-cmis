608a3d390f23de29a0ae5d07cf0e887aeabb2e1a
Paul Holden <pholden@stria.com>
2018-08-07 09:40:34 -0700

    Implement mkdir -p

--- cmis/mkdir.py
+++ cmis/mkdir.py
@@ -4,13 +4,40 @@
 # Connect to a CMIS server and create a folder or nested folders
 ##
 
+import os
+from cmislib.exceptions import ContentAlreadyExistsException, UpdateConflictException
 import string
 import util
 
 mkdir = util.create_command()
 
-@mkdir.main('name')
-def mkdir_main(context, name):
+def mkdir_p(context, name):
+    client = util.create_client(context)
+    repo = client.defaultRepository
+    root = repo.getRootFolder()
+
+    folders = string.split(name, '/')
+
+    if len(folders) == 1:
+        try:
+            root.createFolder(name)
+        except UpdateConflictException:
+            pass
+    else:
+        parent = root
+        path = ''
+
+        for sub in folders:
+            path = path + '/' + sub
+
+            try:
+                new_id = parent.createFolder(sub)
+            except UpdateConflictException:
+                new_id = repo.getObjectByPath(path)
+
+            parent = repo.getFolder(new_id)
+
+def mkdir_nop(context, name):
     client = util.create_client(context)
     repo = client.defaultRepository
     root = repo.getRootFolder()
@@ -25,3 +52,14 @@ def mkdir_main(context, name):
         for sub in folders:
             new_id = parent.createFolder(sub)
             parent = repo.getFolder(new_id)
+
+@mkdir.main('name')
+def mkdir_main(context, name):
+    if 'p' in context:
+        return mkdir_p(context, name)
+    else:
+        return mkdir_nop(context, name)
+
+@mkdir.option('-p')
+def mkdir_p(context, p=True):
+    context['p'] = p
