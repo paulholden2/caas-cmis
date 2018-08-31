491f573e712998cb9ecfa386ca180a994cfa726a
Paul Holden <pholden@stria.com>
2018-08-31 14:50:36 -0700

    Fix ls --root....

--- cmis/ls.py
+++ cmis/ls.py
@@ -11,28 +11,28 @@ ls_cmd = util.create_command()
 ls_cmd.description = 'List repositories on a server or files and folders in a repository.'
 
 @ls_cmd.option('--root')
-def ls_opt_r(context, root=None):
+def ls_opt_root(context, root=None):
     context['root'] = root
 
 @ls_cmd.main('[path]')
 def ls(context, path=None):
     client = util.create_client(context)
 
-    if path is None and context['root'] is None:
+    if path is None and 'root' not in context:
         for repo in client.getRepositories():
             print('Repository: ' + str(repo['repositoryName']) + ' (' + str(repo['repositoryId']) + ')')
 
         return
 
-    path = util.sanitize_path(path)
     repo = client.defaultRepository
 
-    if path == '/' or context['root'] is not None:
+    if path == '/' or 'root' in context:
         root = repo.getRootFolder()
 
         for child in root.getChildren().getResults():
             print(child.getName())
     else:
+        path = util.sanitize_path(path)
         obj = repo.getObjectByPath(path)
 
         for child in obj.getChildren().getResults():
