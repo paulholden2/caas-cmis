f3a3e90f808fbaccda98ca77262428bee2e8d3e8
Paul Holden <pholden@stria.com>
2018-08-31 14:45:17 -0700

    Fix ls -r, rename to --root

--- cmis/ls.py
+++ cmis/ls.py
@@ -10,15 +10,15 @@ ls_cmd = util.create_command()
 
 ls_cmd.description = 'List repositories on a server or files and folders in a repository.'
 
-@ls_cmd.option('-r')
-def ls_opt_r(context, r=None):
-    context['r'] = r
+@ls_cmd.option('--root')
+def ls_opt_r(context, root=None):
+    context['root'] = root
 
 @ls_cmd.main('[path]')
 def ls(context, path=None):
     client = util.create_client(context)
 
-    if path is None:
+    if path is None and context['root'] is None:
         for repo in client.getRepositories():
             print('Repository: ' + str(repo['repositoryName']) + ' (' + str(repo['repositoryId']) + ')')
 
@@ -27,7 +27,7 @@ def ls(context, path=None):
     path = util.sanitize_path(path)
     repo = client.defaultRepository
 
-    if path == '/' or context['r'] is not None:
+    if path == '/' or context['root'] is not None:
         root = repo.getRootFolder()
 
         for child in root.getChildren().getResults():
